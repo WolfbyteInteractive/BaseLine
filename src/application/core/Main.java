5025ecf7d753cf65cb365f1274658870d6504364
WolfbyteInteractive <wolfbyteInteractive@gmail.com>
2015-01-18 20:13:37 +0000

    Fxml sheet added
    New fxml sheet added and changed packaging

--- src/application/core/Main.java
+++ src/application/core/Main.java
@@ -1,12 +1,18 @@
-package application;
+package application.core;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
+/**
+ * This is the main class. This class will pull everything together
+ * and display all of the components
+ * @author Zack/Wolfbyte
+ *
+ */
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
